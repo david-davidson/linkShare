90cada84976c0e436e772038360717167831b176
David Davidson <daviddavidson.1988@gmail.com>
2014-09-14 21:10:10 -0700

    Add full CRUD methods

--- app/controllers/initController.js
+++ app/controllers/initController.js
@@ -2,17 +2,48 @@
 
 module.exports = function(app) {
 	app.controller('initController',
-		[ '$scope', 'talkToServer',
-		function($scope, talkToServer) {
+		[ '$scope', 'httpService',
+		function($scope, httpService) {
 
+			$scope.newLink = {};
+			$scope.newLink.linkBody = '';
+
+	    // Create
+	    $scope.saveNewLink = function() {
+	    	httpService.post($scope.newLink)
+	    	.success(function() {
+	    		$scope.getAllLinks();
+	    	});
+	    	$scope.newLink.linkBody = ''; // Reset the form
+	    };
+
+			// Read
 			$scope.getAllLinks = function() {
-	      talkToServer.get()
+	      httpService.get()
         .success(function(data) {
            $scope.links = data;
         });
 	    };
-	    $scope.getAllLinks();
+	    $scope.getAllLinks(); // Grab the data when the controller loads
 
-			$scope.message = 'Hello, world!';
+	    // Update
+	    $scope.updateLink = function(link) {
+	    	link.editing = true;
+	    };
+	    $scope.saveOldLink = function(link) {
+	    	httpService.put(link)
+        .success(function() {
+          $scope.getAllLinks();
+        });
+	    };
+
+	    // Delete
+	    $scope.deleteLink = function(link) {
+	    	httpService.delete(link)
+	    	.success(function() {
+	    		$scope.getAllLinks();
+	    	});
+	    };
+
 		} ]);
 };
